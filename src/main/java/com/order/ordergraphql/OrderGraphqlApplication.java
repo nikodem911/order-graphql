5e83dda0d523daaad26dda1819f6b328f1d81755
nikodem911 <nikodem.jura@gmail.com>
2024-01-07 14:34:31 +0100

    Initial commit

---
+++ src/main/java/com/order/ordergraphql/OrderGraphqlApplication.java
@@ -0,0 +1,13 @@
+package com.order.ordergraphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderGraphqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderGraphqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/order/ordergraphql/OrderGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.order.ordergraphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderGraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
